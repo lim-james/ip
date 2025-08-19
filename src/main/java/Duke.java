48ae0e61b2d381dc67276736ff82cc9791630031
James <jameslimbj@gmail.com>
2025-08-19 16:33:55 +0800

    feat: add welcome message
    remove placeholder code
    add welcome and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatbotName = "Peter";
+        String divider = "____________________________________________________________";
+        String welcomeMsg = String.format("""
+ Hello! I'm %s
+ What can I do for you?""", chatbotName);
+        String exitMsg = """
+ Bye. Hope to see you again soon!""";
+
+        System.out.println(divider);
+        System.out.println(welcomeMsg);
+        System.out.println(divider);
+        System.out.println(exitMsg);
+        System.out.println(divider);
+        return;
     }
 }
