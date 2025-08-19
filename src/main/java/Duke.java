e972a5bfd9a4f56fd897024e261bf4c429f15c39
James <jameslimbj@gmail.com>
2025-08-19 16:45:15 +0800

    refactor: abstract display text behaviour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,31 @@
 public class Duke {
-    public static void main(String[] args) {
-        String chatbotName = "Peter";
-        String divider = "____________________________________________________________";
-        String welcomeMsg = String.format("""
+    private static final String chatbotName = "Peter";
+    private static final String divider = "____________________________________________________________";
+
+    private static void displayWelcome() {
+        String welcomeMsg = String.format(
+            """
  Hello! I'm %s
- What can I do for you?""", chatbotName);
+ What can I do for you?""", 
+            chatbotName
+        );
+        System.out.println(divider);
+        System.out.println(welcomeMsg);
+    }
+
+    private static void displayExit() {
         String exitMsg = """
  Bye. Hope to see you again soon!""";
 
         System.out.println(divider);
-        System.out.println(welcomeMsg);
-        System.out.println(divider);
         System.out.println(exitMsg);
         System.out.println(divider);
+    }
+
+    public static void main(String[] args) {
+        displayWelcome();
+        echoLoop();
+        displayExit();
         return;
     }
 }
