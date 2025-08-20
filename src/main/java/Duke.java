26ea27f207a3fa83100a3b69a1b78b2176f67a8e
James <jameslimbj@gmail.com>
2025-08-20 13:26:21 +0800

    feat: add echo loop
    get input -> print input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String chatbotName = "Peter";
     private static final String divider = "____________________________________________________________";
 
+    
     private static void displayWelcome() {
         String welcomeMsg = String.format(
             """
@@ -20,6 +23,22 @@ public class Duke {
         System.out.println(divider);
         System.out.println(exitMsg);
         System.out.println(divider);
+    }
+
+    private static void echoLoop() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(divider);
+        String userInput = sc.nextLine();
+
+        System.out.println(divider);
+
+        while (!userInput.equals("bye")) {
+            System.out.println("> " + userInput);
+            System.out.println(divider);
+            userInput = sc.nextLine();
+        }
+
+        sc.close();
     }
 
     public static void main(String[] args) {
