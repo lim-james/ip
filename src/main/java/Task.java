7bd36f2c2cbab09edafdd1ad29b0737713f69db3
James <jameslimbj@gmail.com>
2025-08-22 14:52:59 +0800

    feat: define Task class
    each class includes - description & isDone flag

---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (this.isDone ? "X" : " ") + "] " + this.description;
+    }
+}
