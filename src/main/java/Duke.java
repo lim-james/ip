1144eb9d23eb0bfa86dc33ba82d2a2f290e1da97
James <jameslimbj@gmail.com>
2025-08-22 15:25:04 +0800

    feat: add mark and unmark user input controls

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -39,10 +39,23 @@ public class Duke {
                 for (var i = 0; i < list.size(); ++i) {
                     System.out.println((i + 1) + ". " + list.get(i).toString());
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                Task task = list.get(index);
+                task.mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task.toString());
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                Task task = list.get(index);
+                task.unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task.toString());
             } else {
                 System.out.println("added: " + input);
                 list.add(new Task(input));
             }
+
             System.out.println(divider);
             input = sc.nextLine();
             System.out.println(divider);
