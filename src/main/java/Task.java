3e0e724c72219419ab0e7a0a334184254330e715
James <jameslimbj@gmail.com>
2025-08-22 15:24:36 +0800

    feat: add mark & unmark class behaviour

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,14 @@ public class Task {
         this.isDone = false;
     }
 
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
     @Override
     public String toString() {
         return "[" + (this.isDone ? "X" : " ") + "] " + this.description;
