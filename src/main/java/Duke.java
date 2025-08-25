b91911c4bb0b4c6f03c7e092ad7ab847bdc265b6
James <jameslimbj@gmail.com>
2025-08-25 11:41:00 +0800

    feat: handle 'IncompleteTaskException' in global run

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Optional;
 
-
 public class Duke {
     private static final String chatbotName = "Peter";
     private static final String divider = "____________________________________________________________";
@@ -27,13 +26,43 @@ public class Duke {
         System.out.println(divider);
     }
 
-    private static Task buildTask(String input) {
-        Task ret = ToDo.build(input);
-        if (ret == null) 
+    private static Task buildTask(String input) throws IncompleteTaskException {
+        Task ret = null;
+        if (ToDo.isMatch(input)) {
+            ret = ToDo.build(input);
+        } else if (Deadline.isMatch(input)) {
             ret = Deadline.build(input);
-        if (ret == null) 
+        } else if (Event.isMatch(input)) {
             ret = Event.build(input);
+        }
         return ret;
+    }
+
+    private static void branch(String input, ArrayList<Task> list) throws IncompleteTaskException {
+        if (input.equals("list")) {
+            for (var i = 0; i < list.size(); ++i) {
+                System.out.println((i + 1) + ". " + list.get(i).toString());
+            }
+        } else if (input.startsWith("mark ")) {
+            int index = Integer.parseInt(input.substring(5)) - 1;
+            Task task = list.get(index);
+            task.mark();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + task.toString());
+        } else if (input.startsWith("unmark ")) {
+            int index = Integer.parseInt(input.substring(7)) - 1;
+            Task task = list.get(index);
+            task.unmark();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + task.toString());
+        } else { 
+            Task task = buildTask(input);
+            if (task != null) {
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + task.toString());
+                list.add(task);
+            }
+        }
     }
 
     private static void run() {
@@ -46,29 +75,11 @@ public class Duke {
         ArrayList<Task> list = new ArrayList<>();
 
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                for (var i = 0; i < list.size(); ++i) {
-                    System.out.println((i + 1) + ". " + list.get(i).toString());
-                }
-            } else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                Task task = list.get(index);
-                task.mark();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + task.toString());
-            } else if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.substring(7)) - 1;
-                Task task = list.get(index);
-                task.unmark();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + task.toString());
-            } else { 
-                Task task = buildTask(input);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + task.toString());
-                list.add(task);
+            try {
+                branch(input, list);
+            } catch (IncompleteTaskException e) {
+                System.out.println("OH DEAR! " + e.getMessage());
             }
-
             System.out.println(divider);
             input = sc.nextLine();
             System.out.println(divider);
