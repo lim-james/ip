d7ecc8e31819a5477021a75c2fd7085d74abe797
James <jameslimbj@gmail.com>
2025-09-08 13:44:20 +0800

    fix: add javadco for 'FindCommand'

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -2,7 +2,19 @@ package duke.command;
 
 import duke.task.TaskList;
 
+/**
+ * Represents a command that searches for tasks in the task list whose descriptions contain a
+ * specified keyword or phrase.
+ */
 public class FindCommand extends Command {
+
+    /**
+     * Executes the command by filtering the task list for tasks that match the given description
+     * keyword or phrase, and prints the results to the console.
+     *
+     * @param list The task list to search within.
+     * @param description The keyword or phrase to match against task descriptions.
+     */
     @Override
     public void execute(TaskList list, String description) {
         System.out.println("Here are the matching tasks in your list:");
